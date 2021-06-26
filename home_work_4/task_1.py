28c70d50f9d381cf076d9b0ca218482a4b73c2c0
ZemiosLemon <khizhnyak.andrej@gmail.com>
2021-06-26 14:44:49 +0300

    done

--- home_work_4/task_1.py
+++ home_work_4/task_1.py
@@ -1,8 +1,11 @@
-x = int(input('run first day'))
-y = int(input())
-sum = 0
-while x < y:
-    x += (0.1*x)
-    sum += 1
-    print(x)
-    print(sum)
+def run_man():
+    x = int(input('run first day'))
+    y = int(input())
+    sum = 0
+    while x < y:
+        x += (0.1*x)
+        sum += 1
+    return sum
+
+
+print(run_man())
