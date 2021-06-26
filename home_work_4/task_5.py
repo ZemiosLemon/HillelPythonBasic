01b590786a37abc044291722d860d677c7cafedd
ZemiosLemon <khizhnyak.andrej@gmail.com>
2021-06-26 18:16:04 +0300

    done

--- home_work_4/task_5.py
+++ home_work_4/task_5.py
@@ -1,12 +1,45 @@
-str = list(range(1, 21))
-print(str)
-print(str[2]) #   a. выведите третий символ этой строки.
-print(str[-2]) # b. выведите предпоследний символ этой строки.
-print(str[0:5]) # c. выведите первые пять символов этой строки.
-print(str[0:-2]) # d. выведите всю строку, кроме последних двух символов.
-print(str[1:-1:2])#    e. выведите все символы с четными индексами (считая, что индексация начинается с 0,
-#    поэтому символы выводятся начиная с первого).
-print(str[0:-1:2])#    f. выведите все символы с нечетными индексами, то есть начиная со второго символа строки.
-print(str[::-1])#    g. выведите все символы в обратном порядке.
-print(str[::-2])#    h. выведите все символы строки через один в обратном порядке, начиная с последнего.
-print(len(str))#   i. выведите длину данной строки.
+def third_character(line: list) -> str:
+    return line[2]
+
+
+def penultimate(line: list) -> str:
+    return line[-2]
+
+
+def firs_five(line: list) -> list:
+    return line[0:5]
+
+
+def without_the_last_two(line: list) -> list:
+    return line[0:-2]
+
+
+def even_numbers(line: list) -> list:
+    return line[2:-1:2]
+
+
+def odd_numbers(line: list) -> list:
+    return line[1:-1:2]
+
+
+def reverse_list(line: list) -> list:
+    return line[::-1]
+
+
+def reverse_in_one(line: list) -> list:
+    return line[::-2]
+
+
+def lengths_list(line: list) -> int:
+    return len(line)
+
+
+def main(some_line: list):
+    call_functions = [third_character(some_line), penultimate(some_line), firs_five(some_line),
+                      without_the_last_two(some_line), even_numbers(some_line), odd_numbers(some_line),
+                      reverse_list(some_line), reverse_in_one(some_line), lengths_list(some_line)]
+    for num in call_functions:
+        print(num)
+
+
+main(list(range(0, 21)))
