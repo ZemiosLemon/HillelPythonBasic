5d5bb1772a281cf0412484f90ca286ade1340393
ZemiosLemon <khizhnyak.andrej@gmail.com>
2021-07-01 17:40:55 +0300

    done

--- home_work_5/task_4.py
+++ home_work_5/task_4.py
@@ -1,19 +1,19 @@
 from random import randint
 
-some_list = [randint(10, 20) for i in range(10)]
+random_list = [randint(10, 20) for i in range(10)]
 
 
 def change_num(some_list: list):
     new_list = []
-    sum = 0
+    summa = 0
     for num in some_list:
         if num % 2 == 0:
             new_list.append(num)
         else:
             new_list.append(0)
-            sum += 1
-    return new_list, sum
+            summa += 1
+    return new_list, summa
 
-
-print(change_num(some_list)[0])
-print(change_num(some_list)[1])
+print(random_list)
+print(change_num(random_list)[0])
+print(change_num(random_list)[1])
