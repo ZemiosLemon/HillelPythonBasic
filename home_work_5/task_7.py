4e8add4729ed491d7bbc03af81dfd12e59250442
ZemiosLemon <khizhnyak.andrej@gmail.com>
2021-07-02 14:17:17 +0300

    done

--- home_work_5/task_7.py
+++ home_work_5/task_7.py
@@ -1,7 +1,18 @@
 import datetime
+d = int(input())
+m = int(input())
+y = int(input())
 
 
-# def is_date(day: int, month: int, year: int):
-#     datetime.date()
 
-print(datetime.date())
+
+
+def is_date(date: int, month: int, year: int):
+try:
+    date_obj = datetime.date(y, m, d)
+    return True
+except ValueError:
+    return False
+
+
+#date_obj-date.today()
