6694b6d60ed2bacad1ae6707dad7ed47ee940f9b
ZemiosLemon <khizhnyak.andrej@gmail.com>
2021-07-20 22:33:04 +0300

    done

---
+++ homework_7/task_2.py
@@ -0,0 +1,15 @@
+temperature = int(input())
+type_temp = input()
+
+
+
+#def calc_temp():
+if type_temp == 'c':
+    f = temperature * 9 / 5 + 32
+    k = temperature + 273.15
+
+
+    print(f, k, temperature)
+#elif type_temp == 'f'
+
+
