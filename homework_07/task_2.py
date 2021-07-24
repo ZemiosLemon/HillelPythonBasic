3575584a725864878c6cc840deb3bd5bccff67d8
ZemiosLemon <khizhnyak.andrej@gmail.com>
2021-07-24 14:42:51 +0300

    done

--- homework_07/task_2.py
+++ homework_07/task_2.py
@@ -1,15 +1,20 @@
-temperature = int(input())
-type_temp = input()
+temp = int(input('Введите температуру:'))
+type_temp = input('Введите тип температуры:')
 
 
+def calc_temp(temperature: int, type_temperature: str) -> None:
+    if type_temperature == 'C':
+        f = temperature * 9 / 5 + 32
+        k = temperature + 273.15
+        print(f'Цельсий: {temperature}\nФаренгейт: {f}\nКельвин: {k}')
+    elif type_temperature == 'F':
+        c = (temperature - 32)/1.8
+        k = (temperature - 32) * 5/9 + 273.15
+        print(f'Цельсий: {c}\nФаренгейт: {temperature}\nКельвин: {k}')
+    else:
+        f = (temperature - 273.15) * 9/5 + 32
+        c = temperature - 273.15
+        print(f'Цельсий: {c}\nФаренгейт: {f}\nКельвин: {temperature}')
 
-#def calc_temp():
-if type_temp == 'c':
-    f = temperature * 9 / 5 + 32
-    k = temperature + 273.15
 
-
-    print(f, k, temperature)
-#elif type_temp == 'f'
-
-
+calc_temp(temp, type_temp)
