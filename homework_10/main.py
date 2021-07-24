d1bc280103d1dcbda3965510528c3d2beabbf5d1
KhizhnyakAndrey <85702272+ZemiosLemon@users.noreply.github.com>
2021-07-24 14:11:47 +0300

    Delete homework_10 directory

--- homework_10/main.py
+++
@@ -1,4 +0,0 @@
-words = 'What makes a good man'
-
-line = words.split()
-print(max(line, key=len))
--- homework_10/task_2.py
+++
@@ -1,9 +0,0 @@
-mail = input()
-
-
-def hide_email(email: str) -> str:
-    split_email = email.split('@')
-    return f'{split_email[0][0:-3]}***@**{split_email[1][2:]}'
-
-
-print(hide_email(mail))
