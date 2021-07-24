06798902791e1c8f4da1da5071c7a729f1ab871f
ZemiosLemon <khizhnyak.andrej@gmail.com>
2021-07-24 12:57:52 +0300

    done

--- рomeцork_2/Ex#6.py
+++
@@ -1,7 +0,0 @@
-enter_num = float(input())
-num = int(abs(enter_num))
-new_list = tuple(range(10, 101, 10))
-if abs(num) not in new_list:
-    print(f'Число {enter_num} в списке отсутствует')
-else:
-    print(f'Число {num} присутствует в списке')
