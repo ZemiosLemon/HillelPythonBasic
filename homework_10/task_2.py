60b12a6968661eb485eb1ed5fd0b1853215b50da
ZemiosLemon <khizhnyak.andrej@gmail.com>
2021-07-26 13:34:45 +0300

    done

--- homework_10/task_2.py
+++ homework_10/task_2.py
@@ -2,8 +2,8 @@ mail = input()
 
 
 def hide_email(email: str) -> str:
-    split_email = email.split('@')
-    return f'{split_email[0][0:-3]}***@**{split_email[1][2:]}'
+    first_part, second_part = email.split('@')
+    return f'{first_part[:-3]}***@**{second_part[2:]}'
 
 
 print(hide_email(mail))
--- homework_10/task_3.py
+++ homework_10/task_3.py
@@ -1,20 +1,22 @@
 longest_word_line = 'What makes a good man'
 
 
-def longest_word(line: str) -> str:
-    split_line = line.split()
-    new_list = []
-    max_word = len(split_line[0])
-    for num in split_line:
-        new_list.append(len(num))
-        if len(num) > max_word:
-            max_word = len(num)
-            return split_line[new_list.index(max_word)]
+#def longest_word(line: str) -> str:
+split_line = line.split()
+new_list = []
+max_word = len(split_line[0])
+for num in split_line:
+    new_list.append(len(num))
+
+    if len(num) > max_word:
+        max_word = len(num)
+        print(filter(filter(max_word, split_line))
+        #return split_line[new_list.index(max_word)]
 
 
-def cheat_longest_word(line: str) -> str:
-    return max(line.split(), key=len)
+# def cheat_longest_word(line: str) -> str:
+#     return max(line.split(), key=len)
 
 
-print(longest_word(longest_word_line))
-print(cheat_longest_word(longest_word_line))
+# print(longest_word(longest_word_line))
+# print(cheat_longest_word(longest_word_line))
