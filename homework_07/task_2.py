59a122f47235d486e8c50142b6385580104beaa6
ZemiosLemon <khizhnyak.andrej@gmail.com>
2021-07-26 17:29:17 +0300

    done

--- homework_07/task_2.py
+++ homework_07/task_2.py
@@ -2,27 +2,24 @@ temp = int(input('Введите температуру:'))
 type_temp = input('Введите тип температуры:')
 
 
-def calc_celsius(temperature: int) -> (int, int, int):
-    celsius = temperature
-    fahrenheit = temperature * 9 / 5 + 32
-    kelvin = temperature + 273.15
-    print(f'Цельсий: {celsius}\nФаренгейт: {fahrenheit}\nКельвин: {kelvin}')
+def calc_temp(temperature: int, type_temperature: str) -> None:
+    if type_temperature == 'C':
+        celsius = temperature
+        fahrenheit = temperature * 9 / 5 + 32
+        kelvin = temperature + 273.15
+        print(f'Цельсий: {celsius}\nФаренгейт: {fahrenheit}\nКельвин: {kelvin}')
+    elif type_temperature == 'F':
+        fahrenheit = temperature
+        celsius = (temperature - 32)/1.8
+        kelvin = (temperature - 32) * 5/9 + 273.15
+        print(f'Цельсий: {celsius}\nФаренгейт: {fahrenheit}\nКельвин: {kelvin}')
+    elif type_temperature == 'K':
+        kelvin = temperature
+        fahrenheit = (temperature - 273.15) * 9/5 + 32
+        celsius = temperature - 273.15
+        print(f'Цельсий: {celsius}\nФаренгейт: {fahrenheit}\nКельвин: {kelvin}')
+    else:
+        print('Error')
 
 
-def calc_fahrenheit(temperature: int) -> (int, int, int):
-    fahrenheit = temperature
-    celsius = (temperature - 32)/1.8
-    kelvin = (temperature - 32) * 5/9 + 273.15
-    print(f'Цельсий: {celsius}\nФаренгейт: {fahrenheit}\nКельвин: {kelvin}')
-
-
-def calc_kelvin(temperature: int) -> (int, int, int):
-    kelvin = temperature
-    fahrenheit = (temperature - 273.15) * 9/5 + 32
-    celsius = temperature - 273.15
-    print(f'Цельсий: {celsius}\nФаренгейт: {fahrenheit}\nКельвин: {kelvin}')
-
-
-
-if type_temp == 'C':
-    print(f'Цельсий: {calc_celsius}\nФаренгейт: {calc_celsius}\nКельвин: {calc_celsius}')
+calc_temp(temp, type_temp)
