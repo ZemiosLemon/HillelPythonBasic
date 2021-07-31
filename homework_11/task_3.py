1e205141fc73d8e6a5f2c687cc515977d63af538
ZemiosLemon <khizhnyak.andrej@gmail.com>
2021-07-31 22:39:33 +0300

    done

--- homework_11/task_3.py
+++ homework_11/task_3.py
@@ -1,11 +1,12 @@
 list_user = list(range(10))
 n = int(input())
 
-def shift_elements(line: list, nn: int):
-    n = -n % len(line)
+
+def shift_elements(line: list, num: int):
+    num = -num % len(line)
     return list_user[n:] + list_user[:n]
 
 
-shift_elements(list_user, n)
+print(shift_elements(list_user, n))
 
 
