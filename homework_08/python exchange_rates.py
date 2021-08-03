2d0280c46b9e48b79fdc83189416df8888bf27d6
ZemiosLemon <khizhnyak.andrej@gmail.com>
2021-08-03 11:41:33 +0300

    done

--- homework_08/python exchange_rates.py
+++ homework_08/python exchange_rates.py
@@ -4,12 +4,13 @@ import requests
 import datetime
 import json
 
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Currency exchange app')
     parser.add_argument('currency_from')
     parser.add_argument('currency_to')
     parser.add_argument('amount', type=float)
-    parser.add_argument('-sd', '--start_date', default=datetime.datetime.now())
+    parser.add_argument('-sd', '--start_date', default=datetime.date.today())
     args = parser.parse_args()
 
 
@@ -34,15 +35,14 @@ def symbols_check():
 
 def exchange():
     exchange_from, exchange_to = symbols_check()[0], symbols_check()[1]
-    final_list = [['date', 'from', 'to', 'amount', 'rate', 'result']]
-    date_start = date_check()
+    final_list = [["date", "from", "to", "amount", "rate", "result"]]
+    date_start = datetime.datetime.strptime(args.start_date, "%Y-%m-%d")
     while date_start < datetime.datetime.now():
         get_list = requests.get('https://api.exchangerate.host/convert',
                                 params={'from': exchange_from, 'to': exchange_to,
                                         'amount': args.amount, 'date': date_start}).json()
         date_start += datetime.timedelta(days=1)
-        result_list = [get_list['date'], exchange_from, exchange_to, args.amount, get_list['info']['rate'],
-                       get_list['result']]
+        result_list = [get_list['date'], exchange_from, exchange_to, get_list['info']['rate'], get_list['result']]
         final_list.append(result_list)
         time.sleep(1)
     for num in final_list:
